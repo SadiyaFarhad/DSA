1552f24fc7e36d6e0e02c6875dc6e55c48723e75
Sadiya Farhad <sadiyafarhad709@gmail.com>
2024-10-15 20:47:24 +0530

    Add files via upload

---
+++ Arithmetic_operation.c
@@ -0,0 +1,15 @@
+
+
+class arithmetic_Op
+{
+public static void main (String args[]){
+int a=10;
+System.out.println((a++) +" "+ (++a));
+System.out.println((a--) +" "+ (--a));
+
+System.out.println(a-b);//5
+System.out.println(a*b);//50
+System.out.println(a/b);//2
+System.out.println(a%b);//0
+}}
+  
